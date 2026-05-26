dbca3e055edf405bda4a93aa09f3e8b3e5d50217
1299016404-netizen <1299016404-netizen@users.noreply.github.com>
2026-05-26 20:58:28 +0800

    build: lazy fonts (page renders instantly even on throttled networks)

--- _next/static/chunks/app/layout-8ccee5db8937fcf1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},2310:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1290,23)),Promise.resolve().then(n.bind(n,8720))}},e=>{e.O(0,[819,720,441,255,358],()=>e(e.s=2310)),_N_E=e.O()}]);
